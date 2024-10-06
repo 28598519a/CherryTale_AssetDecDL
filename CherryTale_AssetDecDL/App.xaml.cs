ca782baf6f409c8fe39f4253294d04ec42afeabc
劃破黑夜 <179971b@gmail.com>
2024-10-06 20:21:17 +0800

    v1.5

--- CherryTale_AssetDecDL/App.xaml.cs
+++ CherryTale_AssetDecDL/App.xaml.cs
@@ -13,7 +13,6 @@ namespace CherryTale_AssetDecDL
         public static string Respath = String.Empty;
         public static int TotalCount = 0;
         public static int glocount = 0;
-        // ping ohgf.uquqp.com => 107.155.58.212
         public static string ServerURL = "https://jifd.yayyf.com/patch/files/";
         public static List<string> log = new List<string>();
     }
