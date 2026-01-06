6a49551f2cfec1be2dd1f735b10254f136ad1983
劃破黑夜 <179971b@gmail.com>
2026-01-06 23:31:16 +0800

    Update ServerURL

--- CherryTale_AssetDecDL/App.xaml.cs
+++ CherryTale_AssetDecDL/App.xaml.cs
@@ -13,7 +13,8 @@ namespace CherryTale_AssetDecDL
         public static string Respath = String.Empty;
         public static int TotalCount = 0;
         public static int glocount = 0;
-        public static string ServerURL = "https://jifd.yayyf.com/patch/files/";
+        public static string ServerURL = "https://hefc.hrbzqy.com/patch/files/";
         public static List<string> log = new List<string>();
     }
 }
+
